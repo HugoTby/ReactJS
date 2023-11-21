af634bac9da7dbeb93eaf20cd73a7b5eb387716e
HugoTby <hugotabarylapro@gmail.com>
2023-11-21 11:26:15 +0100

    commit

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +18,37 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}*/
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First App</h1>
+      {/* <input type="text" placeholder='username'/><br></br>
+      <input type="password" placeholder='password' /><br style={{ marginTop: '5px' }}></br>
+      <input type="submit" value="Se connecter" style={{ marginTop: '15px' }} /> */}
+      <img src={logo} className="App-logo" style={{ marginTop: '225px' }} alt="logo" /><div class="login-box">
+        <h2 style={{ fontSize: '25px' }}>Login</h2>
+        <form>
+          <div class="user-box">
+            <input type="text" name="" required="" />
+            <label>Username</label>
+          </div>
+          <div class="user-box">
+            <input type="password" name="" required="" />
+            <label>Password</label>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </a>
+        </form>
+      </div>
     </div>
   );
 }
